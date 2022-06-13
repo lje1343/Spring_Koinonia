6dd4c270f601ad453f8893649d5bddfeb9b4bb5b
clouououd <wjdrhks02050@gmail.com>
2022-06-13 15:17:17 +0900

    220613 로그인, 회원가입 유효성 검사 완료

--- src/main/resources/static/assets/js/join.js
+++ src/main/resources/static/assets/js/join.js
@@ -49,6 +49,7 @@ const checkName = () => {
     $("#name").attr("style", "border-color:#e00751;");
     $("#nameGuidMsg").text("이름을 입력해주세요.");
     $("#nameGuidMsg").attr("style", "display:block;");
+    return false;
   } else {
     $("#name").removeAttr("style");
     $("#nameGuidMsg").removeAttr("style");
@@ -58,16 +59,20 @@ const checkName = () => {
     $("#name").attr("style", "border-color:#e00751;");
     $("#nameGuidMsg").text("이름에는 특수문자를 사용할 수 없습니다.");
     $("#nameGuidMsg").attr("style", "display:block;");
+    return false;
   } else {
     $("#name").removeAttr("style");
     $("#nameGuidMsg").removeAttr("style");
   }
-  // if ($("#name").val().length < 2 || $("#name").val().length > 12) {
-  //   console.log("s");
-  //   $("#name").attr("style", "border-color:#e00751;");
-  //   $("#nameGuidMsg").text("이름은 2자이상 12자이하만 가능합니다.");
-  //   $("#nameGuidMsg").attr("style", "display:block;");
-  // }
+  if ($("#name").val().length < 2 || $("#name").val().length > 12) {
+    $("#name").attr("style", "border-color:#e00751;");
+    $("#nameGuidMsg").text("이름은 2자이상 12자이하만 가능합니다.");
+    $("#nameGuidMsg").attr("style", "display:block;");
+    return false;
+  } else {
+    $("#name").removeAttr("style");
+    $("#nameGuidMsg").removeAttr("style");
+  }
   // 중복 확인
   // joinService.checkDuplication($("#name").val(), (result) => {
   //   if (result) {
@@ -77,6 +82,7 @@ const checkName = () => {
   //     $("#nameGuidMsg").attr("style", "display:block;");
   //   }
   // });
+  return true;
 };
 const checkEmail = () => {
   // null 여부 확인
@@ -84,6 +90,7 @@ const checkEmail = () => {
     $("#email").attr("style", "border-color:#e00751;");
     $("#emailGuidMsg").text("이메일을 입력해주세요.");
     $("#emailGuidMsg").attr("style", "display:block;");
+    return false;
   } else {
     $("#email").removeAttr("style");
     $("#emailGuidMsg").removeAttr("style");
@@ -93,6 +100,7 @@ const checkEmail = () => {
     $("#email").attr("style", "border-color:#e00751;");
     $("#emailGuidMsg").text("올바른 이메일 주소를 입력해주세요");
     $("#emailGuidMsg").attr("style", "display:block;");
+    return false;
   } else {
     $("#email").removeAttr("style");
     $("#emailGuidMsg").removeAttr("style");
@@ -106,6 +114,7 @@ const checkEmail = () => {
   //     $("#emailGuidMsg").attr("style", "display:block;");
   //   }
   // });
+  return true;
 };
 const checkPw = () => {
   // null 여부 확인
@@ -113,41 +122,80 @@ const checkPw = () => {
     $("#pw").attr("style", "border-color:#e00751;");
     $("#pwGuidMsg").text("비밀번호를 입력해주세요.");
     $("#pwGuidMsg").attr("style", "display:block;");
+    return false;
   } else {
     $("#pw").removeAttr("style");
     $("#pwGuidMsg").removeAttr("style");
   }
   // 구성 조건
-  if (pw.test($("#name").val())) {
+  console.log(pw.test($("#pw").val()));
+  if (!pw.test($("#pw").val())) {
     $("#pw").attr("style", "border-color:#e00751;");
     $("#pwGuidMsg").text(
       "비밀번호는 영문 대소문,숫자 조합으로 4~12자리를 사용해야 합니다."
     );
     $("#pwGuidMsg").attr("style", "display:block;");
+    return false;
   } else {
     $("#pw").removeAttr("style");
     $("#pwGuidMsg").removeAttr("style");
   }
+  return true;
+};
+// 필수 체크사항 체크 확인
+// 이용약관
+const checkInputcheck01 = () => {
+  if (!$("#check01").is(":checked")) {
+    $("#check01GuidMsg").text("약관을 읽고 동의해야 합니다.");
+    $("#check01GuidMsg").attr("style", "display:block;");
+    return false;
+  } else {
+    $("#check01GuidMsg").removeAttr("style");
+  }
+  return true;
+};
+// 개인정보수집동의
+const checkInputcheck02 = () => {
+  if (!$("#check02").is(":checked")) {
+    $("#check02GuidMsg").text("개인정보 수집ㆍ이용에 대한 동의를 해야합니다.");
+    $("#check02GuidMsg").attr("style", "display:block;");
+    return false;
+  } else {
+    $("#check02GuidMsg").removeAttr("style");
+  }
+  return true;
 };
 
 $(document).ready(() => {
   // 이름
   $("#name").on("change", () => {
-    checkName();
+    console.log(checkName());
   });
   // 이메일
   $("#email").on("change", () => {
-    checkEmail();
+    console.log(checkEmail());
   });
   // 비밀번호
   $("#pw").on("change", () => {
-    checkPw();
+    console.log(checkPw());
+  });
+  // 체크박스
+  $("#check01").on("change", () => {
+    console.log(checkInputcheck01());
+  });
+  $("#check02").on("change", () => {
+    console.log(checkInputcheck02());
   });
 });
-$("#submit").on("click", (e) => {
-  e.preventDefault();
-  checkName();
-  checkEmail();
-  checkPw();
-  $("form").submit();
-});
+const dobuleCheck = () => {
+  if (
+    checkName() &&
+    checkEmail() &&
+    checkPw() &&
+    checkInputcheck01() &&
+    checkInputcheck02()
+  ) {
+    return true;
+  }
+  return false;
+};
