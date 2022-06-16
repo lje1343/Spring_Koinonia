bb208da44683d1c9d1cbc8e051b4f8e441fc35e5
Eunoia030 <105345279+Eunoia030@users.noreply.github.com>
2022-06-16 10:52:27 +0900

    .....
    ......

---
+++ spring-project/src/main/java/com/example/teamproject/domain/vo/RequestReplyVO.java
@@ -0,0 +1,10 @@
+package com.example.teamproject.domain.vo;
+
+public class RequestReplyVO {
+    private int Rrno;
+    private int Rno;
+    private String Content;
+    private String Name;
+    private String RegisterDate;
+    private String UpdateDate;
+}
