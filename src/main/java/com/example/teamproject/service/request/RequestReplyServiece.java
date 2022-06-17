6ded14318079f21b59643eb8a0a8d036577391ba
karisuma23 <gieobn@gmail.com>
2022-06-17 14:53:06 +0900

    no message

--- src/main/java/com/example/teamproject/service/request/RequestReplyServiece.java
+++ src/main/java/com/example/teamproject/service/request/RequestReplyServiece.java
@@ -1,6 +1,6 @@
-package com.example.teamproject.service.request;
-
-import com.example.teamproject.service.ReplyService;
-
-public class RequestReplyServiece implements ReplyService {
-}
+//package com.example.teamproject.service.request;
+//
+//import com.example.teamproject.service.ReplyService;
+//
+//public class RequestReplyServiece implements ReplyService {
+//}
--- src/test/java/com/example/teamproject/mapper/UserMapperTests.java
+++ src/test/java/com/example/teamproject/mapper/UserMapperTests.java
@@ -1,6 +1,6 @@
 package com.example.teamproject.mapper;
 
-import com.example.teamproject.domain.vo.BoardVO;
+import com.example.teamproject.domain.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +15,9 @@ public class UserMapperTests {
 
     @Test
     public void insertTest(){
-        BoardVO boardVO = new BoardVO();
-        boardVO.setBoardTitle("새로운 게시글 제목2");
-        boardVO.setBoardContent("새로운 게시글 내용2");
-        boardVO.setBoardWriter("hds1111");
 
-        boardMapper.insert(boardVO);
 
-        log.info("게시글 번호 : " + boardVO.getBoardBno());
+
     }
 
 
