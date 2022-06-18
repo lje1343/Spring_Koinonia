d93e1279222e086f0026979bf7debd4ca35f9dee
karisuma23 <gieobn@gmail.com>
2022-06-18 18:48:42 +0900

    보드완성

--- src/main/java/com/example/teamproject/domain/vo/ProductReplyVO.java
+++ src/main/java/com/example/teamproject/domain/vo/ProductReplyVO.java
@@ -1,10 +1,15 @@
 package com.example.teamproject.domain.vo;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
 public class ProductReplyVO {
-    private int Prno;
-    private int Pno;
-    private String Content;
-    private String Name;
-    private String RegisterDate;
-    private String UpdateDate;
+    private int prno;
+    private int pno;
+    private String content;
+    private String name;
+    private String registerDate;
+    private String updateDate;
 }
--- src/main/java/com/example/teamproject/domain/vo/RequestReplyVO.java
+++ src/main/java/com/example/teamproject/domain/vo/RequestReplyVO.java
@@ -1,10 +1,15 @@
 package com.example.teamproject.domain.vo;
 
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
 public class RequestReplyVO {
-    private int Rrno;
-    private int Rno;
-    private String Content;
-    private String Name;
-    private String RegisterDate;
-    private String UpdateDate;
+    private int rrno;
+    private int rno;
+    private String content;
+    private String name;
+    private String registerDate;
+    private String updateDate;
 }
