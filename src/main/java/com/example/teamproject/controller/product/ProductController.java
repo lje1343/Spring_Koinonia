be58ea0e51a822b922c47149e914f91c6afc8244
rhgurqo52 <rhgurqo52@naver.com>
2022-06-21 08:47:21 +0900

    presentaion 1차 수정

--- src/main/java/com/example/teamproject/controller/product/ProductController.java
+++ src/main/java/com/example/teamproject/controller/product/ProductController.java
@@ -25,7 +25,7 @@ public class ProductController {
         log.info("*************");
         log.info("판매 상품정보 작성");
         log.info("*************");
-        return "/product/register";
+        return "/product/product";
     }
 //    @PostMapping("/register")
 //    public String register(ProductVO productVO, RedirectAttributes rttr){
